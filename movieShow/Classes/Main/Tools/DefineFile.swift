375469edb90b14675cc3bcf769af3cd997259fd9
何怡家 <heyijia@betty.local>
2018-11-29 00:34:45 +0800

    添加首页标签栏

---
+++ movieShow/Classes/Main/Tools/DefineFile.swift
@@ -0,0 +1,17 @@
+//
+//  DefineFile.swift
+//  movieShow
+//
+//  Created by 何怡家 on 2018/11/28.
+//  Copyright © 2018 zhangyanlf.cn. All rights reserved.
+//
+
+import UIKit
+
+
+let kStatusBarH: CGFloat = 30
+let kNavBarH: CGFloat = 50
+
+// 屏幕
+let kScreenW = UIScreen.main.bounds.width
+let kScreenH = UIScreen.main.bounds.height
