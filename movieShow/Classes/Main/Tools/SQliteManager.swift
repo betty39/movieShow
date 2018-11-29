079152fa8bb195eeedc1c810e46a7d5f4834e52b
haha <14301133@bjtu.edu.cn>
2018-11-29 23:54:07 +0800

    暂存

--- movieShow/Classes/Main/Tools/SQliteManager.swift
+++ movieShow/Classes/Main/Tools/SQliteManager.swift
@@ -25,10 +25,18 @@ struct SQLiteManager {
     ///用户 表
     private let movies_user = Table("movies_user")
     
-    //表字段
+    ///收藏的电影 表
+    private let movies_like = Table("movies_like")
+    
     let id = Expression<Int64>("id")
     let username = Expression<String>("username")
     let password = Expression<String>("password")
+    //表字段
+    let movieid = Expression<String>("movieid")
+    let title = Expression<String>("title")
+    let average = Expression<String>("average")
+    let imgmedium = Expression<String>("imgmedium")
+    let year = Expression<String>("year")
     ///创建表
     func tableLampCreate() -> Void {
             try! database.run(movies_user.create(ifNotExists: true, block: { t in
@@ -81,4 +89,40 @@ struct SQLiteManager {
         // 取出数据库中数据
         let title = movies_user.filter(username == newsTitle.username)
     }
+    
+    
+    ///创建电影收藏表
+    func tablemovlike() -> Void {
+        try! database.run(movies_like.create(ifNotExists: true, block: { t in
+            t.column(id, primaryKey: .autoincrement)
+            t.column(username)
+            t.column(movieid)
+            t.column(title)
+            t.column(average)
+            t.column(imgmedium)
+            t.column(year)
+        }))
+    }
+    func insertmov(_ mov : MovieLike) {
+        // 如果数据库中该条数据不存在， 插入
+        if !existmov(mov) {
+            let insert = movies_user.insert(username <- mov.username, movieid <- mov.movieid, title <- mov.title, average <- mov.average, imgmedium <- mov.imgmedium, year <- mov.year)
+            //插入数据
+            try! database.run(insert)
+            print("插入数据！")
+        }
+        
+    }
+    
+    /// 判断数据库中某一条电影喜欢数据是否存在
+    func existmov(_ movie: MovieLike) -> Bool {
+        // 取出该的数据
+        let title = movies_like.filter(username == movie.username && movieid == movie.movieid)
+        // 判断该条数据是否存在，没有直接的方法
+        // 可以根据 count 是否是 0 来判断是否存在这条数据，0 表示没有该条数据，1 表示存在该条数据
+        let count = try! database.scalar(title.count)
+        
+        return count != 0
+    }
+    
 }
